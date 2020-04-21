25ff28ba7cd30a399d664760eeddac03ca3ce54f
Inalberth Pinheiro <inalberth07@gmail.com>
2020-04-21 10:50:50 -0300

    Implementação de controle de cache e ETag.

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -4,36 +4,70 @@ import com.algaworks.algafood.api.model.mapper.FormaPagamentoMapper;
 import com.algaworks.algafood.api.model.request.FormaPagamentoRequest;
 import com.algaworks.algafood.api.model.response.FormaPagamentoResponse;
 import com.algaworks.algafood.domain.model.FormaPagamento;
+import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.domain.service.FormaPagamentoService;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import javax.validation.Valid;
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/formas-pagamento")
 public class FormaPagamentoController {
 
     final FormaPagamentoService service;
+    final FormaPagamentoRepository repository;
     final FormaPagamentoMapper mapper;
 
-    public FormaPagamentoController(FormaPagamentoService service, FormaPagamentoMapper mapper) {
+    public FormaPagamentoController(FormaPagamentoService service, FormaPagamentoRepository repository,
+                                    FormaPagamentoMapper mapper) {
         this.service = service;
+        this.repository = repository;
         this.mapper = mapper;
     }
 
     @GetMapping
-    public List<FormaPagamentoResponse> listar() {
+    public ResponseEntity<List<FormaPagamentoResponse>> listar(ServletWebRequest request) {
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+        String eTag = "0";
+        OffsetDateTime dataUltimaAtualizacao = repository.getUltimaDataAtualizacao();
+
+        if (dataUltimaAtualizacao != null)
+            eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+
+        if (request.checkNotModified(eTag))
+            return null;
+
         List<FormaPagamento> formasPagamento = service.listar();
-        return mapper.toCollectionResponse(formasPagamento);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                .body(mapper.toCollectionResponse(formasPagamento));
     }
 
     @GetMapping("/{formaPagamentoId}")
-    public FormaPagamentoResponse buscar(@PathVariable Long formaPagamentoId) {
+    public ResponseEntity<FormaPagamentoResponse> buscar(@PathVariable Long formaPagamentoId,
+                                                         ServletWebRequest request) {
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+        String eTag = "0";
+        OffsetDateTime dataUltimaAtualizacao = repository.getDataAtualizacaoById(formaPagamentoId);
+
+        if (dataUltimaAtualizacao != null)
+            eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+
+        if (request.checkNotModified(eTag))
+            return null;
+
         FormaPagamento formaPagamento = service.buscarOuFalhar(formaPagamentoId);
-        return mapper.toResponse(formaPagamento);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                .body(mapper.toResponse(formaPagamento));
     }
 
     @PostMapping
--- src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -2,8 +2,17 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
 
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+
+    @Query("select max(dataAtualizacao) from FormaPagamento")
+    OffsetDateTime getUltimaDataAtualizacao();
+
+    @Query("select dataAtualizacao from FormaPagamento where id = :formaPagamentoId")
+    OffsetDateTime getDataAtualizacaoById(Long formaPagamentoId);
 }
